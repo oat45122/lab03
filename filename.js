6ea46c405339f77c051aa5d950f8ce5f33f16ff4
oat45122 <woradon.thawornsusin@mail.kmutt.ac.th>
2021-02-15 02:38:58 +0700

    first

---
+++ filename.js
@@ -0,0 +1,4 @@
+NAME = prompt('What is your favorite charactor?')
+COLOR = prompt('What is the main color of '+ NAME)
+console.log(NAME + ' is your favorite charactor')
+console.log(NAME + ' is ' + COLOR )
